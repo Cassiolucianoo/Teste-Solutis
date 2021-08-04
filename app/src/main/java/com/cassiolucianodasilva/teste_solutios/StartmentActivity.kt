c9de1a2adbac9abefa8b7c2759300206feb4135a
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-08-04 17:47:50 -0300

    xml activity
    criação de toda estrutura dos xml  das activity

---
+++ app/src/main/java/com/cassiolucianodasilva/teste_solutios/StartmentActivity.kt
@@ -0,0 +1,11 @@
+package com.cassiolucianodasilva.teste_solutios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startment)
+    }
+}
